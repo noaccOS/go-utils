94622f58da825923a700ec800bbb865ab4a401dc
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-07-24 17:33:52 +0200

    feat: update shas to include major and minor
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- util/update_sha/update-sha.sh
+++ util/update_sha/update-sha.sh
@@ -4,8 +4,7 @@ function go_vers() {
     echo '{'
     git ls-remote --tags --refs https://github.com/golang/go.git 'go*' |
         sort -V -u -r -k2 |
-        sed -r 's/^(\w+)\s+(refs\/tags\/go(([0-9]+)\.([0-9]+).*))$/"\3":{"rev":"\1","ref":"\2","version":"\3","minor":"\4_\5"}/' |
-        sed -r 's/^(\w+)\s+(refs\/tags\/go(([0-9]+).*))$/"\3":{"rev":"\1","ref":"\2","version":"\3","minor":"\4_0"}/' |
+        sed -r 's/^(\w+)\s+(refs\/tags\/go(([0-9]+)(\.([0-9]+))?.*))$/"\3":{"rev":"\1","ref":"\2","version":"\3","major":"\4","minor":"\6"}/' |
         paste -sd ','
     echo '}'
 }
